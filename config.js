88ea44d2449db2c5692cd02d24e97bd0dbb86734
12mahboob <152901209+12mahboob@users.noreply.github.com>
2024-10-01 09:07:04 +0500

    added files

---
+++ config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDIXTdmPeQZeIS7mmA2E06vg_ow4ZWAOT0",
+  authDomain: "ytube-earn.firebaseapp.com",
+  databaseURL: "https://ytube-earn-default-rtdb.firebaseio.com",
+  projectId: "ytube-earn",
+  storageBucket: "ytube-earn.appspot.com",
+  messagingSenderId: "179827157577",
+  appId: "1:179827157577:web:bad466f447c859ecdeed98",
+  measurementId: "G-LK3NPV3L3E"
+};
